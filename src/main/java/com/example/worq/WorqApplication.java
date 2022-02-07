4cc456e4a0c674193ebc8d92e357f7b820f05465
leejw <leejw@arainhr.com>
2022-02-07 19:07:06 +0900

    fdf

---
+++ src/main/java/com/example/worq/WorqApplication.java
@@ -0,0 +1,13 @@
+package com.example.worq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/worq/WorqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
